f336d89b4b95c70bf4740036c773500d5aa45d9f
Supriya1782007 <Sujnyaniparayyatelaginamani@gmail.com>
2026-09-11 14:37:14 +0530

    Fix syntax errors in voting eligibility check

--- Vote.py
+++ Vote.py
@@ -1,6 +1,8 @@
-Age = int(input("Enter your age: "))
+Age = int(input("Enter your age:"))
 
-if age >= 18;
+if age>= 18:
     print("You are eligible to vote.")
+    print ("You can register as a voter.")
 else:
     print("You are not eligible to vote.")
+    print("you must be at least 18 year old.")
